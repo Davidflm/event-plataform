5c8f1b4a72e2d9e14e6b6739cdde65fc37b41f22
David Flavio de Lima Menezes <davidf@net.unilab.edu.br>
2022-06-24 10:55:24 -0300

    Inciando parte do video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,26 @@
 export function Video() {
     return (
         <div className="flex-1">
-            Area do Player
+            <div className="bg-black flex justify-center">
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+            </div>
+            <div className="p-8 max-w-[1100px]  mx-auto">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1>
+                            Aula 01  - Abertura do Ignite Lab
+                        </h1>
+                        <p>
+                            Descrição do evento deve ser inserida aqui. Seguir aulas com projetos para programar na pratica ao mesmo tempo em que conhece a teoria é uma ótima estratégia para aprendizado, pois além do conhecimento podemos já ir montando um portifólio.
+                        </p>
+                    </div>
+                    <div></div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 
